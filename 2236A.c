1e42d83fcd174eb860a85670c6ed1a3c053b07e0
Samapti Das <dassomapti05@gmail.com>
2026-06-12 23:21:57 +0600

    file added

---
+++ 2236A.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    int t,n;
+    scanf("%d",&t);
+    //int arr[n];
+    if(t>=1 && t<=100000){
+    while(t--){
+
+            scanf("%d",&n);
+            if(n>=1 && n<=5){
+            int arr[n];
+        for(i=0;i<n;i++){
+            scanf("%d",&arr[i]);
+        }
+        for(i=1;i<n;i++){
+            int current_element=arr[i];
+            j=i-1;
+            while(j>=0 && arr[j]>current_element){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=current_element;
+        }
+       int max_height=arr[n-1]+1;
+       int min_value_of_k=max_height-arr[0];
+
+       printf("%d\n",min_value_of_k);
+
+    }
+    }
+    }
+        return 0;
+}
