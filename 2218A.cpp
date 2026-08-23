82e68d3f07c452941c70f370adc7abbcdc1f55b4
Samapti Das <dassomapti05@gmail.com>
2026-08-23 23:47:58 +0600

    2218A added

---
+++ 2218A.cpp
@@ -0,0 +1,21 @@
+
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t; cin>>t;
+    int x,y;
+    if(t>=1 && t<=6767)
+    while (t--){
+        cin>>x;
+        if(x>=-67 && x<=67){
+            y=x;
+            cout<<y<<endl;
+        }
+
+
+    }
+    return 0;
+}
