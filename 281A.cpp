2a879ced6253f03a28e98a20bb090596fa432ac8
Samapti Das <dassomapti05@gmail.com>
2026-08-28 21:04:58 +0600

    281A added

---
+++ 281A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    string str;
+    cin>>str;
+
+        str[0]=toupper(str[0]);
+
+
+
+    cout<<str<<endl;
+    return 0;
+}
